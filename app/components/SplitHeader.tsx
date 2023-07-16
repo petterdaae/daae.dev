6848ea25c75322d1027a27a95159eae8ad788ab5
Petter Daae <petter.daae@bekk.no>
2023-07-16 11:56:24 +0200

    Create header

---
+++ app/components/SplitHeader.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  left: string;
+  right: string;
+};
+
+export default function SplitHeader({ left, right }: Props) {
+  return (
+    <h1 className="uppercase font-bold text-6xl">
+      <span className="text-black">{left}</span>
+      <span className="text-red">{right}</span>
+    </h1>
+  );
+}
