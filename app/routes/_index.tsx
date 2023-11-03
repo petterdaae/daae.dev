e93e5ed346f2559867e22aaead18c4876f01873a
Petter Daae <petter.daae@bekk.no>
2023-11-04 00:32:05 +0100

    Make it possible to have more pages

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,17 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import PortableText from "react-portable-text";
 import Experience from "~/components/Experience";
 import SplitHeader from "~/components/SplitHeader";
 import { getPage } from "~/lib/sanity";
 
-export const loader = async () => {
-  return await getPage();
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const page = await getPage(url.pathname);
+  if (!page) {
+    throw new Response(null, { status: 404, statusText: "Not Found" });
+  }
+  return page;
 };
 
 export default function Index() {
