609f4e2c5a1cb74c89c15e58069d5ec5d7deb2c4
Petter Daae <petter.daae@bekk.no>
2023-11-04 11:23:36 +0100

    Add codeblock

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -2,5 +2,6 @@ import page from './documents/page'
 import experience from './objects/experience'
 import splitHeader from './objects/splitHeader'
 import altImage from './objects/altImage'
+import codeBlock from './objects/codeBlock'
 
-export const schemaTypes = [page, experience, splitHeader, altImage]
+export const schemaTypes = [page, experience, splitHeader, altImage, codeBlock]
