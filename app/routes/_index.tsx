c4bb0aec90fd5336032220e247c32393c59646bc
Petter Daae <petter.daae@gmail.com>
2024-10-20 12:02:24 +0200

    Use block instead of marks for default text elements

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -28,20 +28,10 @@ export default function Index() {
             experience: Experience,
             altImage: AltImage,
           },
-          marks: {
-            h1: (props: any) => (
-              <h1
-                className="text-3xl lg:text-6xl font-bold pt-8 pb-8"
-                {...props}
-              />
-            ),
-            h2: (props: any) => (
-              <h2 className="text-2xl font-bold pt-4 pb-4" {...props} />
-            ),
-            normal: (props: any) => <p className="pb-2" {...props} />,
-            code: (props: any) => (
-              <code className="bg-rose-300 rounded-lg pl-1 pr-1" {...props} />
-            ),
+          block: {
+            h2: ({ children }) => <h2 className="text-2xl font-bold pt-4 pb-4">{children}</h2>,
+            h1: ({ children }) => <h1 className="text-3xl lg:text-6xl font-bold pt-8 pb-8">{children}</h1>,
+            normal: ({ children }) => <p className="pb-2">{children}</p>,
           }
         }}
       />
