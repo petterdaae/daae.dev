611ef828913e9e1476c857c82b06db1224a3d839
Petter Daae <petter.daae@gmail.com>
2024-10-20 12:01:29 +0200

    Create header

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "@remix-run/react";
+import SplitHeader from "~/components/SplitHeader";
 import { experiencesQuery, getClient } from "~/lib/sanity";
 
 export const loader = async () => {
@@ -9,14 +10,8 @@ export const loader = async () => {
 export default function Index() {
   const { experiences } = useLoaderData();
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Welcome to daae.dev!</h1>
-      <h2>Experience</h2>
-      <ul>
-        {experiences.map((experience: any) => (
-          <li key={experience._id}>{experience.company}</li>
-        ))}
-      </ul>
+    <div className="flex justify-center pt-32">
+      <SplitHeader left="daae" right=".dev" />
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#252a34",
+        red: "#ff2e63",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
