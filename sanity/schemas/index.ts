086cc6601b4af65dec669d250389af5e13846d07
Petter Daae <petter.daae@gmail.com>
2024-10-20 12:01:31 +0200

    Add feature toggles

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,4 +1,5 @@
 import about from './about'
 import experience from './experience'
+import toggles from './toggles'
 
-export const schemaTypes = [about, experience]
+export const schemaTypes = [about, experience, toggles]
