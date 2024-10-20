1dc22a2cdfd693b6c8c8bb88815ee5d48198da4d
Petter Daae <petter.daae@gmail.com>
2024-10-20 12:01:29 +0200

    Create first schema in sanity

---
+++ sanity/schemas/experience.ts
@@ -0,0 +1,12 @@
+export default {
+  name: 'experience',
+  type: 'document',
+  title: 'Experience',
+  fields: [
+    {
+      name: 'company',
+      type: 'string',
+      title: 'Company',
+    },
+  ],
+}
