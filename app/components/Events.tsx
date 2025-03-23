98e069d9d62b9f8fae0b5273b3511859673e6b0f
Petter Daae <petter.daae@gmail.com>
2025-03-23 15:02:47 +0100

    Mobile friendly date and time

--- app/components/Events.tsx
+++ app/components/Events.tsx
@@ -1,19 +1,31 @@
 import P from "./P";
 import H2 from "./H2";
 
+function Keyword(props: { keyword: string }) {
+  return (
+    <span className="p-1 pl-2 pr-2 text-xs rounded-full bg-blue border border-blue mr-2">
+      {props.keyword}
+    </span>
+  );
+}
+
 function Event({ concert }: { concert: any }) {
     const date = new Date(concert.date);
     const month = date.toLocaleDateString('default', { month: 'short' });
     const formattedDate = `${date.getDate()} ${month.toUpperCase()}`;
     return (
-        <div className="flex sm:items-center pb-8">
-            <div className="flex-col bg-blue rounded-lg justify-items-center content-center w-24 h-24 sm:w-32 sm:h-32 mr-4">
+        <div className="flex sm:items-center sm:pb-8">
+            <div className="hidden sm:block flex-col bg-blue rounded-lg justify-items-center content-center w-24 h-24 sm:w-32 sm:h-32 mr-4">
                 <P>{concert.time}</P>
                 <P className="text-l sm:text-xl">{formattedDate}</P>
             </div>
 
             <div className="flex-2 mr-4">
                 <H2 className="text-xl font-bold pb-2">{concert.title}</H2>
+                <div className="mb-2 sm:hidden">
+                    <Keyword keyword={formattedDate} />
+                    <Keyword keyword={concert.time} />
+                </div>
                 <P className="pb-4 sm:pb-0">{concert.description}</P>
                 {!concert.hideButton && <a className="bg-blue rounded-full p-2 pl-4 pr-4 sm:hidden" href={concert.buttonLink}>{concert.buttonText}</a>}
             </div>
@@ -23,8 +35,6 @@ function Event({ concert }: { concert: any }) {
 }
 
 export default function Events({ concerts }: { concerts: any }) {
-    const firsrtConcerts = concerts[1];
-
     const upcommingConcerts = concerts
         .filter((concert: any) => new Date(concert.date) >= new Date())
         .sort((a: any, b: any) => a.date.localCompare(b.date));
@@ -33,10 +43,6 @@ export default function Events({ concerts }: { concerts: any }) {
     const previousConcerts = concerts
         .filter((concert: any) => new Date(concert.date) < new Date())
         .sort((a: any, b: any) => b.date.localeCompare(a.date));
-
-    console.log(previousConcerts);
-
-
 
     return <div className="pt-8">
         {upcommingConcerts.length > 0 && <H2 className="text-2xl pb-4">Kommende konserter</H2>}
